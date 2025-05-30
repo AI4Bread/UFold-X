25a5da214f20250f5d7832d08c439be93ef2ce6a
lee01210 <166137514+lee01210@users.noreply.github.com>
2025-05-30 21:41:21 +0800

    Add files via upload

--- side_Network_for_UFold_X.py
+++ side_Network_for_UFold_X.py
@@ -121,6 +121,72 @@ class U_Net(nn.Module):
 
         return torch.transpose(d1, -1, -2) * d1
 
+class U_Net_for_mamba(nn.Module):
+    def __init__(self,img_ch=3,output_ch=1):
+        super(U_Net_for_mamba,self).__init__()
+        
+        ##self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)  
+        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)  
+        ##self.Upsample = nn.Upsample(scale_factor=2)
+        self.Conv1 = conv_block(ch_in=img_ch,ch_out=int(32*CH_FOLD2))
+        self.Conv2 = conv_block(ch_in=int(32*CH_FOLD2),ch_out=int(64*CH_FOLD2))
+        self.Conv3 = conv_block(ch_in=int(64*CH_FOLD2),ch_out=int(128*CH_FOLD2))
+        self.Conv4 = conv_block(ch_in=int(128*CH_FOLD2),ch_out=int(256*CH_FOLD2))
+        self.Conv5 = conv_block(ch_in=int(256*CH_FOLD2),ch_out=int(512*CH_FOLD2))
+
+        self.Up5 = up_conv(ch_in=int(512*CH_FOLD2),ch_out=int(256*CH_FOLD2))
+        self.Up_conv5 = conv_block(ch_in=int(512*CH_FOLD2), ch_out=int(256*CH_FOLD2))
+
+        self.Up4 = up_conv(ch_in=int(256*CH_FOLD2),ch_out=int(128*CH_FOLD2))
+        self.Up_conv4 = conv_block(ch_in=int(256*CH_FOLD2), ch_out=int(128*CH_FOLD2))
+        
+        self.Up3 = up_conv(ch_in=int(128*CH_FOLD2),ch_out=int(64*CH_FOLD2))
+        self.Up_conv3 = conv_block(ch_in=int(128*CH_FOLD2), ch_out=int(64*CH_FOLD2))
+        
+        self.Up2 = up_conv(ch_in=int(64*CH_FOLD2),ch_out=int(32*CH_FOLD2))
+        self.Up_conv2 = conv_block(ch_in=int(64*CH_FOLD2), ch_out=int(32*CH_FOLD2))
+
+        self.Conv_1x1 = nn.Conv2d(int(32*CH_FOLD2),output_ch,kernel_size=1,stride=1,padding=0)
+
+
+    def forward(self,x):
+        # encoding path
+        x1 = self.Conv1(x)
+        x2 = self.Maxpool(x1)
+        x2 = self.Conv2(x2)
+        
+        x3 = self.Maxpool(x2)
+        x3 = self.Conv3(x3)
+
+        x4 = self.Maxpool(x3)
+        x4 = self.Conv4(x4)
+
+        x5 = self.Maxpool(x4)
+        x5 = self.Conv5(x5)
+
+        # decoding + concat path
+        d5 = self.Up5(x5)
+        d5 = torch.cat((x4,d5),dim=1)
+        
+        d5 = self.Up_conv5(d5)
+        
+        d4 = self.Up4(d5)
+        d4 = torch.cat((x3,d4),dim=1)
+        d4 = self.Up_conv4(d4)
+
+        d3 = self.Up3(d4)
+        d3 = torch.cat((x2,d3),dim=1)
+        d3 = self.Up_conv3(d3)
+
+        d2 = self.Up2(d3)
+        d2 = torch.cat((x1,d2),dim=1)
+        d2 = self.Up_conv2(d2)
+
+        d1 = self.Conv_1x1(d2)
+        d1 = d1.squeeze(1)
+
+        return torch.transpose(d1, -1, -2) * d1
+
 class PatchEmbed2D(nn.Module):
     def __init__(self, patch_size=4, in_chans=17, embed_dim=32, norm_layer=None, **kwargs):
         super().__init__()
