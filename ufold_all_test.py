9b1a62b6896bc324b5f216686294b1c0fcde4ef3
Li Jiachun <166137514+lee01210@users.noreply.github.com>
2025-11-13 17:47:50 +0800

    Update ufold_all_test.py

--- ufold_all_test.py
+++ ufold_all_test.py
@@ -64,7 +64,6 @@ def get_ct_dict_fast(predict_matrix,batch_num,ct_dict,dot_file_dict,seq_embeddin
     seq_letter=''.join([letter[item] for item in np.nonzero(seq_embedding)[:,1]])
     dot_file_dict[batch_num] = [(seq_name,seq_letter,dot_list[:len(seq_letter)])]
     return ct_dict,dot_file_dict
-# randomly select one sample from the test set and perform the evaluation
 
 def contact_map_to_bpseq(contact_map, seq_ori, seq_name, seq_lens):
 
@@ -89,7 +88,7 @@ def contact_map_to_bpseq(contact_map, seq_ori, seq_name, seq_lens):
             
 def model_eval_all_test(contact_net,test_generator):
     ##device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
     #device = torch.device(":1" if torch..is_available() else "cpu")
     contact_net.train()
     result_no_train = list()
@@ -118,13 +117,12 @@ def model_eval_all_test(contact_net,test_generator):
             print('Batch number: ', batch_n)
         
         batch_n += 1
-
         
-        #if batch_n-1 in rep_ind:
-        #    continue
+        #if batch_n > 5:
+        #    break
+        
         contacts_batch = torch.Tensor(contacts.float()).to(device)
         seq_embedding_batch = torch.Tensor(seq_embeddings.float()).to(device)
-        ##seq_embedding_batch_1 = torch.Tensor(seq_embeddings_1.float()).to(device)
         seq_ori = torch.Tensor(seq_ori.float()).to(device)
         # matrix_reps_batch = torch.unsqueeze(
         seq_names.append(seq_name[0])
@@ -134,10 +132,8 @@ def model_eval_all_test(contact_net,test_generator):
         tik = time.time()
         
         with torch.no_grad():
-            #pred_contacts = contact_net(seq_embedding_batch,seq_embedding_batch_1)
             pred_contacts = contact_net(seq_embedding_batch)
 
-        # only post-processing without learning
         u_no_train = postprocess(pred_contacts,
             seq_ori, 0.01, 0.1, 100, 1.6, True,1.5) ## 1.6
             #seq_ori, 0.01, 0.1, 100, 1.6, True) ## 1.6
@@ -148,100 +144,40 @@ def model_eval_all_test(contact_net,test_generator):
         tok = time.time()
         t0 = tok - tik
         run_time.append(t0)
-##      add fine tune
-        #threshold = 0.5
-        '''
-        while map_no_train.sum(axis=1).max() > 1:
-            u_no_train = postprocess(u_no_train,seq_ori, 0.01, 0.1, 50, 1.0, True)
-            threshold += 0.005
-            map_no_train = (u_no_train > threshold).float()
-        '''
-        ## end fine tune
-        #pdb.set_trace()
-        #ct_dict_all = get_ct_dict(map_no_train,batch_n,ct_dict_all)
-        #ct_dict_all,dot_file_dict = get_ct_dict_fast(map_no_train,batch_n,ct_dict_all,dot_file_dict,seq_ori.cpu().squeeze(),seq_name[0])
-        #ct_dict_all,dot_file_dict = get_ct_dict_fast((contacts>0.5).float(),batch_n,ct_dict_all,dot_file_dict,seq_ori.cpu().squeeze(),seq_name[0])
-        #pdb.set_trace()
-        
         result_no_train_tmp = list(map(lambda i: evaluate_exact_new(map_no_train.cpu()[i],
             contacts_batch.cpu()[i]), range(contacts_batch.shape[0])))
-        #print(seq_lens.item())
-        #print(result_no_train_tmp)
-        #print(result_no_train_tmp[0][0])
         result_accuracy.append(result_no_train_tmp[0][2])
         num_len+=1
-        #pdb.set_trace()
         result_no_train += result_no_train_tmp
         
         f1_score=result_no_train_tmp[0][2]
+        ## convert to bpseq
         contact_map_to_bpseq(map_no_train, seq_ori, seq_name, seq_lens)
-        '''
-        with open("0_1_UNetMamba_trainall_1800_name_length_f1.txt", "a") as file1:
-            file1.write(f"{seq_name[0]} {seq_lens.item()} {f1_score}\n")
-        '''
+
         if nc_map_nc.sum() != 0:
             #pdb.set_trace()
             result_nc_tmp = list(map(lambda i: evaluate_exact_new(map_no_train_nc.cpu()[i],
                 nc_map_nc.cpu().float()[i]), range(contacts_batch.shape[0])))
             result_nc += result_nc_tmp
             nc_name_list.append(seq_name[0])
-            #if seq_lens.item() < 400 and result_nc_tmp[0][2] > 0.7:
-                #pdb.set_trace()
-            #    print(seq_name[0])
-            #    print(result_no_train_tmp[0])
-            #    print(result_nc_tmp[0])
-        #seq_lens_list += list(seq_lens)
 
-    #pdb.set_trace()
     print(np.mean(run_time))
     
-    #dot_ct_file = open('results/dot_ct_file.txt','w')
-    '''
-    dot_ct_file = open('results/nonredundant_true_dot_ct_file.txt','w')
-    for i in range(batch_n):
-        dot_ct_file.write('>%s\n'%(dot_file_dict[i+1][0][0]))
-        dot_ct_file.write('%s\n'%(dot_file_dict[i+1][0][1]))
-        #dot_ct_file.write('%s\n'%(dot_file_dict[i+1][0][2]))
-        #dot_ct_file.write('\n')
-    dot_ct_file.close()
-    pdb.set_trace()
-    '''
-    '''
-    ct_file = open('results/ct_file.txt','w')
-    for i in range(batch_n):
-        ct_file.write('>%d\n'%(i))
-        for j in range(len(ct_dict_all[i+1])):
-            ct_file.write('%d\t%d\n'%(ct_dict_all[i+1][j][0],ct_dict_all[i+1][j][1]))
-        ct_file.write('\n')
-    ct_file.close()
-    '''
     nt_exact_p,nt_exact_r,nt_exact_f1 = zip(*result_no_train)
     #pdb.set_trace()
     file_path = '/root/autodl-tmp/UFold/result_accuracy.txt' 
     result_accuracy = [tensor.item() for tensor in result_accuracy]
     print(len(result_accuracy))
     
-    '''
-    np.savetxt(file_path, result_accuracy, fmt='%.6f')
-    '''
-    
     print('Average testing F1 score with pure post-processing: ', np.average(nt_exact_f1))
     print('Average testing precision with pure post-processing: ', np.average(nt_exact_p))
     print('Average testing recall with pure post-processing: ', np.average(nt_exact_r))
     print('Numbers of test seqs:', num_len)
 
-    #with open('/data2/darren/experiment/ufold/results/sample_result.pickle','wb') as f:
-    #    pickle.dump(result_dict,f)
-    # with open('../results/rnastralign_short_pure_pp_evaluation_dict.pickle', 'wb') as f:
-    #     pickle.dump(result_dict, f)
-
 
 def main():
     torch.multiprocessing.set_sharing_strategy('file_system')
     #torch..set_device(0)
-    
-    #pdb.set_trace()
-
     config_file = args.config
     test_file = args.test_files
     
@@ -249,18 +185,8 @@ def main():
     #print('Here is the configuration of this run: ')
     #print(config)
     MODEL_SAVED = 'models/UFold-X_total_train_32.pt'
-    '''
-    if test_file not in ['TS1','TS2','TS3']:
-        MODEL_SAVED = 'models/UFold-X_total_train_32.pt'
-        
-        #MODEL_SAVED = 'models/ufold_train.pt'
-    else:
-        ##MODEL_SAVED = 'models/ufold_train_pdbfinetune.pt'
-        MODEL_SAVED = 'models/UFold_long_train_13.pt'
-    '''
-    
+
     # os.environ["_VISIBLE_DEVICES"]= config.gpu
-    
     d = config.u_net_d
     BATCH_SIZE = config.batch_size_stage_1
     OUT_STEP = config.OUT_STEP
@@ -268,15 +194,15 @@ def main():
     
     # if gpu is to be used
     ##device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #device = torch.device(":2" if torch..is_available() else "cpu")
+    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
     seed_torch()
+    
+    #test file
     print('Loading test file: ',test_file)
-    if test_file == 'RNAStralign' or test_file == '1800':
+    if test_file == 'RNAStralign-1800' or test_file == '1800':
         test_data = RNASSDataGenerator('data/', 'test_no_redundant_1800.pickle')
-        ##test_data = RNASSDataGenerator('/root/autodl-tmp/UFold/data/', 'ArchiveII.pickle')
     elif test_file == '600' or test_file == 'ArchiveII':
-        test_data = RNASSDataGenerator('/root/autodl-tmp/UFold/data/', 'ArchiveII.pickle')
+        test_data = RNASSDataGenerator('data/', 'ArchiveII.pickle')
     else:
         test_data = RNASSDataGenerator('data/',test_file+'.cPickle')
         
@@ -285,50 +211,29 @@ def main():
     print(test_data.data_y.shape)
     pdb.set_trace()
     
-    # using the pytorch interface to parallel the data generation and model training
     params = {'batch_size': BATCH_SIZE,
               'shuffle': True,
               'num_workers': 6,
               'drop_last': True}
 
-    # test_set = Dataset(test_data)
     test_set = Dataset_FCN(test_data)
     test_generator = data.DataLoader(test_set, **params)
     
-
-    ##contact_net = FCNNet(img_ch=17)
     unet = UNet(img_ch=17)  
     vssm = VSSMBranch()
     alpha = 0.5  
     contact_net=UFoldXall(unet=unet, vssm=vssm, alpha=alpha)
-    #contact_net = FCNNet()
-    #contact_net = UNext(input_channels=17)
-    #pdb.set_trace()
     print('==========Start Loading==========')
-    #contact_net.load_state_dict(torch.load(MODEL_SAVED,map_location=':2'))
     ##contact_net.load_state_dict(torch.load(MODEL_SAVED,map_location='cuda:1'))
-    contact_net.load_state_dict(torch.load(MODEL_SAVED,map_location='cuda:0'))
-    #contact_net.load_state_dict(torch.load(MODEL_SAVED,map_location={'0':':2'}))
+    contact_net.load_state_dict(torch.load(MODEL_SAVED,map_location='cuda:2'))
     print('==========Finish Loading==========')
-    # contact_net = nn.DataParallel(contact_net, device_ids=[3, 4])
     contact_net.to(device)
     model_eval_all_test(contact_net,test_generator)
     
     
-    
-    # if LOAD_MODEL and os.path.isfile(model_path):
-    #     print('Loading u net model...')
-    #     contact_net.load_state_dict(torch.load(model_path))
-    
-    
-    # u_optimizer = optim.Adam(contact_net.parameters())
 if __name__ == '__main__':
-    """
-    See module-level docstring for a description of the script.
-    """
     RNA_SS_data = collections.namedtuple('RNA_SS_data','seq ss_label length name pairs')
     main()
-
 
 
 
